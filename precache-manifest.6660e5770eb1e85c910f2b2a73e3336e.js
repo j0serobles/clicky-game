18e515598570a84fd49e47287be0b3283a8e96f1
Jose Robles <jrrs@computer.org>
2019-10-23 16:22:17 -0400

    Updates

---
+++ precache-manifest.6660e5770eb1e85c910f2b2a73e3336e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfbad268407c0cc118e22c0cbc66b4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6699977f3faebe0b3c6f",
+    "url": "/static/css/main.730d2b2f.chunk.css"
+  },
+  {
+    "revision": "d67b6f0d34889443bcbb",
+    "url": "/static/js/2.a88aecb4.chunk.js"
+  },
+  {
+    "revision": "6699977f3faebe0b3c6f",
+    "url": "/static/js/main.32ac90c0.chunk.js"
+  },
+  {
+    "revision": "1f1e643385e17360f83c",
+    "url": "/static/js/runtime-main.c2d72319.js"
+  }
+]);
